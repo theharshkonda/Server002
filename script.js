9b55320dd5736b4c7660c23ba5b394ecc6ee4c33
theharshkonda <theharshkonda@gmail.com>
2024-09-22 00:21:19 +0530

    Complete Java, Spring, and Microservices course

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('generate-btn').addEventListener('click', function() {
-    var input = document.getElementById('qr-input').value;
-    if (input.trim() !== "") {
-        var qrUrl = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + encodeURIComponent(input);
-        document.getElementById('qr-code').innerHTML = '<img src="' + qrUrl + '" alt="QR Code">';
-    } else {
-        alert("Please enter text to generate a QR code.");
-    }
-});
